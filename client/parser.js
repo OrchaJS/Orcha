f564c8b9dc7e1b77c7f68a935f9c06d6dcc1f7d4
Christopher Wong <cwong8257@gmail.com>
2018-08-20 15:38:41 -0400

    Refactor simple config workflow, and add eslint

--- client/parser.js
+++ client/parser.js
@@ -1,47 +1,56 @@
-function executeWorkflow(flowObject) {
-  const { StartAt: startAt, States: states } = flowObject;
+// function executeParallel(state, input) {}
 
-  let currentFunc = startAt;
-  let currentObject = states[currentFunc];
-  let nextFunc;
+function executeStateObject(state = {}, input, prevFunc, currFunc) {
+  const type = state.Type;
 
-  // Default prepend
-  let output = `
-  graph TB
-  Start((Start))
-  Start-.-> ${startAt}`;
-
-  let keepGoing = true;
-
-  while (keepGoing) {
-    output += `
-    ${currentFunc}
-    `;
-
-    if (currentObject) {
-      nextFunc = currentObject.Next;
-      output += `
-      ${currentFunc} --> ${nextFunc}
-      `;
-    }
-
-    currentFunc = currentObject.Next;
-    currentObject = states[currentFunc];
-
-    if (currentObject.End) {
-      keepGoing = false;
-      output += `
-      ${currentFunc}-.->End((End));
-      `;
-    }
+  switch (type) {
+    case 'Task':
+      return drawLine(prevFunc, currFunc);
+    case 'Parallel':
+      return executeParallel(state, input);
+    default:
+      return drawLine(prevFunc, currFunc);
   }
-
-  // Default append
-  output += `
-  classDef green fill:#9f6,stroke:#333,stroke-width:2px;
-  class Start,End green`;
-
-  return output;
 }
 
-module.exports = { executeWorkflow };
+function executeWorkflow(flowObject, input = '', options) {
+  const { StartAt: startAt, States: states } = flowObject;
+  let state = states[startAt];
+  let prevFunc;
+  let currFunc = startAt;
+
+  while (true) {
+    input += executeStateObject(state, input, prevFunc, currFunc);
+    if (state === undefined) break;
+    prevFunc = currFunc;
+    currFunc = state.Next;
+    state = states[currFunc];
+  }
+
+  return input;
+}
+
+function drawLine(a = 'Start', b = 'End') {
+  let line = '-->';
+
+  if (a === 'Start' || b === 'End') line = '-.->';
+
+  return `
+  ${a}${line}${b}`;
+}
+
+function defaults() {
+  return `
+  graph TB
+  Start((Start))
+  End((End))
+  classDef green fill:#9f6,stroke:#333,stroke-width:2px;
+  class Start,End green`;
+}
+
+function startWorkFlow(flowObject) {
+  const output = defaults();
+  return executeWorkflow(flowObject, output, { start: true });
+}
+
+module.exports = { startWorkFlow };
