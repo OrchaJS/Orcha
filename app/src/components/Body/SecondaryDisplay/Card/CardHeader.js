980996bab98747d78981e33ef409db7d19073675
leccih <ses2tst@gmail.com>
2018-08-27 17:23:09 -0400

    add secondaryDisplay card content and styles

--- app/src/components/Body/SecondaryDisplay/Card/CardHeader.js
+++ app/src/components/Body/SecondaryDisplay/Card/CardHeader.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import { MyContext } from '../../../../provider/MyProvider';
 
-const CardHeader = ({ title }) => (
-  <MyContext.Consumer>
-    {({ handleClickSecondaryTab }) => (
-      <div
-        className="card__header heading-secondary"
-        onClick={() => handleClickSecondaryTab(title)}
-      >
-        <span className="card__title">{title}</span>
-      </div>
-    )}
-  </MyContext.Consumer>
-);
+const CardHeader = ({ title, handleClickSecondaryTab, secondaryActiveTab }) => {
+  let className = 'card__header heading-secondary';
+
+  if (title === secondaryActiveTab) {
+    className += ' heading-secondary--active';
+  }
+
+  return (
+    <div className={className} onClick={() => handleClickSecondaryTab(title)}>
+      <span className="card__title">{title}</span>
+    </div>
+  );
+};
 
 export default CardHeader;
